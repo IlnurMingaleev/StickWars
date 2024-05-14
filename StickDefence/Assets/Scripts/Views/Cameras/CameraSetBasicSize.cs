45c561fa298e4b107d087b84e78f0166753d4aeb
IlnurMingaleev <106157251+IlnurMingaleev@users.noreply.github.com>
2024-05-14 17:31:10 +0300

    update camera set basic size

--- StickDefence/Assets/Scripts/Views/Cameras/CameraSetBasicSize.cs
+++ StickDefence/Assets/Scripts/Views/Cameras/CameraSetBasicSize.cs
@@ -4,6 +4,14 @@ namespace Views.Cameras
 {
     public class CameraSetBasicSize : MonoBehaviour
     {
-       
+        private Camera _camera;
+        [SerializeField] private float _sizeCamera;
+
+            private void Start()
+        {
+            _camera = Camera.main;
+
+            if (_camera != null) _camera.orthographicSize = _sizeCamera;
+        }
     }
 }
